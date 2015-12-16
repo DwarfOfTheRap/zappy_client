2386de771b0f210c72cc27233f23a8bb38230837
Francois LE-CORRE <frale-co@e3r10p11.42.fr>
2015-12-16 20:37:02 +0100

    Test with return 1 on failure...

--- test.sh
+++ test.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+error=0
 for filename in test/*.lisp
 do
     echo -e " ----------------- Executing $filename -----------------\n"
-    sbcl --script "$filename"
+
+    output=`sbcl --script "$filename" | grep "TOTAL" | awk '{print $5}'`
+     echo "[$output]"
+    if [[ $output -gt 0 ]]; then
+        #echo $output
+        error=`expr $error + $output`
+    fi
     echo -e "\n"
     #echo -e "\n ----------------- End $filename tests -----------------\n"
 done
-
-#for filename in tests/*.sh
-#do
-#    echo -e " ----------------- Executing $filename -----------------\n"
-#    ./"$filename"
-#    echo -e "\n"
-#    #echo -e "\n ----------------- End $filename tests -----------------\n"
-#done
-
+if [[ $error -gt 0 ]]; then
+    echo "$error errors"
+    exit 1
+fi
