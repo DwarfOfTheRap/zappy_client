778cac8dfc392e818d1425ba4b231ebe42c5d1c8
Francois LE-CORRE <frale-co@e3r10p11.42.fr>
2015-12-20 19:02:10 +0100

    testing done

--- src/player.lisp
+++ src/player.lisp
@@ -1,30 +1,18 @@
-;#-quicklisp package position because sbcl --script launch basic sbcl
-(let ((quicklisp-init (merge-pathnames "~/quicklisp/setup.lisp"
-                                       (user-homedir-pathname))))
-  (when (probe-file quicklisp-init)
-    (load quicklisp-init)))
-
-; load tcp/sockets library quietly
-(with-open-file (*standard-output* "/dev/null" :direction :output
-                                   :if-exists :supersede)
-  (ql:quickload "cl-ppcre"))
-
 ;Variable object: contain the game object list
 (defvar *object* '("food" "linemate" "deraumere" "sibur" "mendiane" "phiras" "thystame"))
 
 (defun get-inventory (str)
-  (let ((case-list (cl-ppcre:all-matches-as-strings "[^}{\(, \)]+" str)))
+  (let ((case-list (cl-ppcre:split ", " (car (cl-ppcre:all-matches-as-strings "[^}{]+" str)))))
     (loop for x in case-list
-          for y = (cl-ppcre:split "^\\s+" x )
-          collect y)
-    )
-  )
+          for y = (cl-ppcre:split "\\s+" x )
+          collect y)))
 
 (defun get-vision (str)
-  (let ((case-list (cl-ppcre:all-matches-as-strings "[^}{\(, \)]+" str)))
-    (loop for x in case-list collect x)))
+  (let ((case-list (cl-ppcre:split ", " (car (cl-ppcre:all-matches-as-strings "[^}{]+" str)))))
+    (loop for x in case-list
+          collect x)))
 
-(print (get-inventory "{superstring 4, super 5, other 8, string 23, otherthing 56, vxcc 6}"))
+
 ;(defun get-response (str vision inventory help)
 ;  (cond
 ;    ((string= "{{" (char str 0))
--- test/player.lisp
+++ test/player.lisp
@@ -1,6 +1,41 @@
 (load "lib/lisp-unit.lisp")
 (use-package :lisp-unit)
 
-;(load "src/player.lisp")
+;#-quicklisp package position because sbcl --script launch basic sbcl
+(let ((quicklisp-init (merge-pathnames "~/quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
 
-;(run-tests)
+; load tcp/sockets library quietly
+(with-open-file (*standard-output* "/dev/null" :direction :output
+                                   :if-exists :supersede)
+  (ql:quickload "cl-ppcre"))
+
+(load "src/player.lisp")
+
+(define-test
+  vision-test-list
+  (assert-equal
+    '("linemate sibur" "phiras phiras" "deraumere" "sibur sibur sibur thystame")
+    (get-vision "{linemate sibur, phiras phiras, deraumere, sibur sibur sibur thystame}"))
+  (assert-equal
+    '("sibur" "nourriture sibur phiras phiras" "nourriture nourriture deraumere" "sibur thystame")
+    (get-vision "{sibur, nourriture sibur phiras phiras, nourriture nourriture deraumere, sibur thystame}"))
+  (assert-equal
+    '("" "nourriture sibur phiras phiras" "nourriture nourriture deraumere" "sibur thystame")
+    (get-vision "{, nourriture sibur phiras phiras, nourriture nourriture deraumere, sibur thystame}"))
+  (assert-equal
+    '("sibur phiras" "" "nourriture nourriture deraumere" "sibur thystame")
+    (get-vision "{sibur phiras, , nourriture nourriture deraumere, sibur thystame}"))
+  )
+
+(define-test
+  inventory-test-list
+  (assert-equal '(("nourriture" "10") ("linemate" "4"))
+                (get-inventory "{nourriture 10, linemate 4}")
+   )
+  )
+
+;(print (get-inventory "{nourriture 4, super 5, other 8, string 23, otherthing 56, vxcc 6}"))
+(run-tests)
