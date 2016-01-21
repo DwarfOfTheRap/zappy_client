74a3c859bab9c0f8ac0a0518d29d4d8f2e316c52
Francois LE-CORRE <frale-co@e3r12p7.42.fr>
2016-01-21 18:36:52 +0100

    correting last bug, sorting item, make a default movement if nothing can be taken inside the view

--- src/player.lisp
+++ src/player.lisp
@@ -3,7 +3,7 @@
 (defvar *inventory-regex*  "^\{nourriture \\d+, linemate \\d+, deraumere \\d+, sibur \\d+, mendiane \\d+, phiras \\d+, thystame \\d+\}$")
 (defvar *broadcast-regex* "^message [1-9], .*$")
 (defvar *push-regex* "^deplacement \\d$")
-; Level up needs
+                                        ; Level up needs
 (defvar *stone-per-level* '((1 0 0 0 0 0) (1 1 1 0 0 0) (2 0 1 0 2 0) (1 1 2 0 1 0) (1 2 1 3 0 0) (1 2 3 0 1 0) (2 2 2 2 2 1)))
 (defvar *symbol-list* '(|nourriture| |linemate| |deraumere| |sibur| |mendiane| |phiras| |thystame|))
 
@@ -15,37 +15,40 @@
 
 (defun make-path-2 (tile element)
   (if (= 0 tile)
-    (return-from make-path-2 (list (concatenate 'string "prend " (symbol-name element))))
-    (append '("avance") (make-path-2 (- tile 2) element))))
+      (return-from make-path-2 (list (concatenate 'string "prend " (symbol-name element))))
+      (append '("avance") (make-path-2 (- tile 2) element))))
 
 (defun make-path (element)
   "create a list of command: take @pair and return @string list"
+  (if (null element)
+      (return-from make-path '("gauche" "avance")) )
   (loop for i from 1 to 7
         for j = 0 then (+ 1 j)
         if (<= (* i i) (cdr element))
-        append '("avance") into ret
+          append '("avance") into ret
         else
-        do (let ((tile (- (cdr element) (* j j))))
-             (if (= 0 tile)
-                 (return-from make-path (append ret (list (concatenate 'string "prend " (symbol-name (car element)))))))
-             (if (oddp tile)
-               (return-from make-path (append ret '("gauche") (make-path-2 (+ 1 tile) (car element))))
-               (return-from make-path (append ret '("droite") (make-path-2 tile (car element))))))))
+          do (let ((tile (- (cdr element) (* j j))))
+               (if (= 0 tile)
+                   (return-from make-path (append ret (list (concatenate 'string "prend " (symbol-name (car element)))))))
+               (if (oddp tile)
+                   (return-from make-path (append ret '("gauche") (make-path-2 (+ 1 tile) (car element))))
+                   (return-from make-path (append ret '("droite") (make-path-2 tile (car element))))))))
 
 (defun search-in-vision (list vision)
-  "for each item in list, search in vision the corresponding key and return the pair (item . tile) "
+  "for each item in list, search in vision the corresponding key and return a list of pair (item . tile) or nil"
   (loop for item in list
         for x = (loop for sub in vision
                       when (member item sub)
-                      return (car sub))
+                        return (car sub))
         if x
-        collect (cons item x)))
+          collect (cons item x) into ret
+        finally (return (sort ret #'< :key #'cdr))))
 
 (defun seek-stone (inventory level)
   "function that check wich object the droid will be looking for"
   (loop for i from 1 to 6
         when (< (second (nth i inventory)) (nth (- i 1) (nth (- level 1) *stone-per-level*)))
-        collect (nth i *symbol-list*)))
+          collect (nth i *symbol-list*)))
 
 (defun check-inventory (inventory level)
   "look if food is needed and seek stones otherwise"
@@ -78,8 +81,8 @@
           for i from 1
           collect item into lst
           when (member i '(1 4 9 16 25 36 49 64))
-          append (organize-line (subseq lst sav) half) into ret
-          and do (progn (incf half) (setf sav i))
+            append (organize-line (subseq lst sav) half) into ret
+            and do (progn (incf half) (setf sav i))
           finally (return ret))))
 
 (defun get-vision (str)
@@ -111,16 +114,16 @@
 
 
 
-;(defun game-loop (newcli socket coord)
-;  "loop with a throttle until it catch a response from server"
-;  (let ((vision '(0)) (inventory base-inv) (command '(0)) (objective '(0))) ;should set inventory with 10f
-;    (loop
-;      (if (listen (usocket:socket-stream socket))
-;        ;(progn
-;        (get-response (read-line (usocket:socket-stream socket)) vision inventory resp msg)
-;        ; )
-;        )
-;      (sleep 0.001)
-;      )
-;    )
-;  )
+                                        ;(defun game-loop (newcli socket coord)
+                                        ;  "loop with a throttle until it catch a response from server"
+                                        ;  (let ((vision '(0)) (inventory base-inv) (command '(0)) (objective '(0))) ;should set inventory with 10f
+                                        ;    (loop
+                                        ;      (if (listen (usocket:socket-stream socket))
+                                        ;        ;(progn
+                                        ;        (get-response (read-line (usocket:socket-stream socket)) vision inventory resp msg)
+                                        ;        ; )
+                                        ;        )
+                                        ;      (sleep 0.001)
+                                        ;      )
+                                        ;    )
+                                        ;  )
